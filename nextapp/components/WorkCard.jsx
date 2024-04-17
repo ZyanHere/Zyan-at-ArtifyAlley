72357f359c95a7158ad194f4240763e6726a3c9f
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-04-17 21:32:21 +0530

    workcard structure completed

--- nextapp/components/WorkCard.jsx
+++ nextapp/components/WorkCard.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import { ArrowBackIosNew, ArrowForwardIos } from "@mui/icons-material";
+import React, { useState } from "react";
 
 const WorkCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [currentindex, setCurrentIndex] = useState(0);
 
-export default WorkCard
+  const goToNextSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex + 1) % work.workPhotoPaths.length
+    );
+  };
+
+  const goToPrevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + work.workPhotoPaths.length) %
+        work.workPhotoPaths.length
+    );
+  };
+
+  return (
+    <div className="work-card">
+      <div className="slider-container">
+        <div className="slider" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+          {Work.workPhotoPaths?.map((photo, index) => (
+            <div className="slide" key={index}>
+              <img src={photo} alt="work" />
+              <div className="prev-button" onClick={(e) => goToPrevSlide(e)}>
+                <ArrowBackIosNew sx={{ fontSize: "15px" }} />
+              </div>
+              <div className="next-button" onClick={(e) => goToNextSlide(e)}>
+                <ArrowForwardIos sx={{ fontSize: "15px" }} />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="info">
+        <div>
+          <h3>{work.title}</h3>
+          <div className="creator">
+            <img src={work.creator.profileImagePath} alt="creator" />
+            <span>{work.creator.usrename}</span> in <span>{work.category}</span>
+          </div>
+        </div>
+        <div className="price">${work.price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default WorkCard;
