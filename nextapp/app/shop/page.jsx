29629d5b6dcb898a647b8c12a208a151c51e77d2
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-04-21 16:04:01 +0530

    shop page and route updated

--- nextapp/app/shop/page.jsx
+++ nextapp/app/shop/page.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+"use client";
+
+import Loader from "@components/Loader";
+import Navbar from "@components/Navbar";
+import WorkList from "@components/WorkList";
+import { useSession } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import "@styles/Shop.scss";
 
 const Shop = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(true);
 
-export default Shop
+  const { data: session } = useSession();
+  const loggedInUserId = session?.user?._id;
+
+  const searchParams = useSearchParams();
+
+  const [workList, setWorkList] = useState([]);
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    const getWorkList = async () => {
+      const response = await fetch(`api/user/${profileId}/shop`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await response.json();
+      setWorkList(data.workList);
+      setProfile(data.user);
+      setLoading(false);
+    };
+
+    if (loggedInUserId) {
+      getWorkList();
+    }
+  }, [loggedInUserId]);
+
+  return loading ? (
+    <Loader />
+  ) : (
+    <>
+      <Navbar />
+
+      {loggedInUserId && <h1 className="title-list">Your Works</h1>}
+
+      {loggedInUserId && (
+        <h1 className="title-list">{profile.username}'s Works</h1>
+      )}
+
+      <WorkList data={workList} />
+    </>
+  );
+};
+
+export default Shop;
